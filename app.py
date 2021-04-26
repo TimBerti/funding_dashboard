77517ab4ce6dde7e69dfbc8c3fb0145ff4ee656b
TimBerti <T.Berti@gmx.net>
2021-04-26 09:54:46 +0200

    3 phases

--- app.py
+++ app.py
@@ -4,97 +4,108 @@ import pandas as pd
 import plotly.graph_objects as go
 from math import ceil
 
-user_count = int(st.text_input('User count:', value=8000))
-time_frame = int(st.text_input('after (in months):', value=24))
-revenue_per_user = float(st.text_input('Revenue per user (in EUR):', value=10))
-monthly_cost = int(st.text_input('Monthly expenses (in EUR):', value=15000))
+def draw_graph(df, break_even, needed_funding):
 
-break_even = time_frame * \
-    np.log(monthly_cost/revenue_per_user) / np.log(user_count)
+    fig = go.Figure()
 
-x = np.arange(0, ceil(break_even) + 2)
-revenue = revenue_per_user * np.exp(x/time_frame*np.log(user_count))
+    fig.add_trace(go.Scatter(
+        x=[df['Month'].iloc[-1]/2],
+        y=[df['Expenses'].mean()/2],
+        mode='text',
+        text=f'Needed Funding: {needed_funding:.2f} EUR',
+        name='Funding',
+        showlegend=False
+    ))
 
-df = pd.DataFrame({'Revenue': revenue, 'Expenses': np.ones(
-    len(x)) * monthly_cost, 'Month': x})
+    fig.add_trace(go.Scatter(
+        x=df['Month'],
+        y=df['Expenses'],
+        line=dict(
+            width=3,
+            color='red'
+        ),
+        name='Expenses'
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=df['Month'],
+        y=df['Revenue'],
+        line=dict(
+            width=3,
+            color='blue'
+        ),
+        name='Revenue'
+    ))
+
+    fig.add_vline(
+        x=break_even,
+        annotation_text=f'Break-even: {break_even:.1f} months',
+        annotation_position='bottom right'
+    )
+
+    fig.update_layout({
+        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+    })
+
+    fig.update_yaxes(
+        title_text='EUR'
+    )
+
+    fig.update_xaxes(
+        title_text='Month'
+    )
+
+    st.plotly_chart(fig)
+
+month = np.array([])
+revenue = np.array([])
+total_time_frame = 0
+
+st.title('Phase 1')
+
+user_count1 = int(st.text_input('1. Final user count:', value=0))
+time_frame1 = int(st.text_input('1. Duration (in months):', value=6))
+revenue_per_user1 = float(st.text_input('1. Revenue per user (in EUR):', value=10))
+monthly_cost1 = int(st.text_input('1. Monthly expenses (in EUR):', value=4000))
+
+
+st.title('Phase 2')
+
+user_count2 = int(st.text_input('2. Final user count:', value=500))
+time_frame2 = int(st.text_input('2. Duration (in months):', value=12))
+revenue_per_user2 = float(st.text_input('2. Revenue per user (in EUR):', value=10))
+monthly_cost2 = int(st.text_input('2. Monthly expenses (in EUR):', value=8000))
+
+
+st.title('Phase 3')
+
+user_count3 = int(st.text_input('3. Final user count:', value=2000))
+time_frame3 = int(st.text_input('3. Duration (in months):', value=12))
+revenue_per_user3 = float(st.text_input('3. Revenue per user (in EUR):', value=10))
+monthly_cost3 = int(st.text_input('3. Monthly expenses (in EUR):', value=15000))
+
+costs = np.concatenate([np.ones(time_frame1) * monthly_cost1,
+                np.ones(time_frame2) * monthly_cost2,
+                np.ones(time_frame3) * monthly_cost3])
+
+revenues = np.concatenate([(0 + np.arange(1, time_frame1 + 1, 1) * (user_count1 - 0) / time_frame1) * revenue_per_user1,
+                    (user_count1 + np.arange(1, time_frame2 + 1, 1) * (user_count2 - user_count1) / time_frame2) * revenue_per_user2,
+                    (user_count2 + np.arange(1, time_frame3 + 1, 1) * (user_count3 - user_count2) / time_frame3) * revenue_per_user3])
+
+month = np.arange(0, time_frame1 + time_frame2 + time_frame3)
+
+df = pd.DataFrame({'Revenue': revenues, 'Expenses': costs, 'Month': month})
+
+print(df)
+
+break_even = 1000000
+
+for index, (revenue, cost) in enumerate(zip(revenues, costs)):
+    if revenue > cost:
+        revenue_growth = revenues[index] - revenues[index - 1]
+        break_even = index - 1 + (costs[index] - revenues[index - 1]) / revenue_growth
 
 needed_funding = df[df['Month'] < break_even]['Expenses'].sum(
 ) - df[df['Month'] < break_even]['Revenue'].sum()
 
-fig = go.Figure()
-
-fig.add_trace(go.Scatter(
-    x=pd.concat(
-        [df[df['Month'] < break_even]['Month'], pd.Series(break_even), df[df['Month'] < break_even]['Month'][::-1]]),
-    y=pd.concat(
-        [df[df['Month'] < break_even]['Revenue'],  pd.Series(monthly_cost), df[df['Month'] < break_even]['Expenses']]),
-    mode='none',
-    fill='toself',
-    fillcolor='red',
-    opacity=.5,
-    name='Losses',
-    showlegend=False
-))
-
-fig.add_trace(go.Scatter(
-    x=pd.concat(
-        [pd.Series(break_even), df[df['Month'] > break_even]['Month'], df[df['Month'] > break_even]['Month'][::-1], pd.Series(break_even)]),
-    y=pd.concat(
-        [pd.Series(monthly_cost), df[df['Month'] > break_even]['Revenue'], df[df['Month'] > break_even]['Expenses'], pd.Series(monthly_cost)]),
-    mode='none',
-    fill='toself',
-    fillcolor='green',
-    opacity=.5,
-    name='Earnings',
-    showlegend=False
-))
-
-fig.add_trace(go.Scatter(
-    x=[break_even / 2],
-    y=[monthly_cost / 2],
-    mode='text',
-    text=f'{needed_funding:.2f} EUR',
-    name='Funding',
-    showlegend=False
-))
-
-fig.add_trace(go.Scatter(
-    x=df['Month'],
-    y=df['Expenses'],
-    line=dict(
-        width=3,
-        color='red'
-    ),
-    name='Expenses'
-))
-
-fig.add_trace(go.Scatter(
-    x=df['Month'],
-    y=df['Revenue'],
-    line=dict(
-        width=3,
-        color='blue'
-    ),
-    name='Revenue'
-))
-
-fig.add_vline(
-    x=break_even,
-    annotation_text=f'Break-even: {break_even:.1f} months',
-    annotation_position='bottom right'
-)
-
-fig.update_layout({
-    'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-})
-
-fig.update_yaxes(
-    title_text='EUR',
-    range=[0, revenue[ceil(break_even) + 1]]
-)
-
-fig.update_xaxes(
-    title_text='Month'
-)
-
-st.plotly_chart(fig)
+draw_graph(df, break_even, needed_funding)
